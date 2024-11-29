9a498cd020d87879aae11c853c6302601f2e734d
Michael Klein <m5l.k3n@gmail.com>
2024-11-29 23:30:14 +0100

    shrink screenshots

--- screenshots/screenshot-android.sh
+++ screenshots/screenshot-android.sh
@@ -8,10 +8,16 @@ if [ ! -f ${ADB} ]; then
     exit 1
 fi
 
+if ! [ -x "$(command -v magick)" ]; then
+    echo "magick not found. Please install imagemagick."
+    exit 1
+fi
+
 vared -p 'Android device plugged in and unlocked? Press Y/y to screenshot its screen: ' -c REPLY
 
 if [[ "$REPLY" =~ ^[Yy]$ ]]; then
     # https://stackoverflow.com/questions/27766712/using-adb-to-capture-the-screen
     ${ADB} exec-out screencap -p > ${TS}.png
+    magick ${TS}.png -resize 50% ${TS}.png
     echo "Android device screenshot saved as: ${TS}.png"
 fi

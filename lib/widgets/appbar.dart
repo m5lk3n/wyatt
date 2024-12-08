4d0d7b24c23e4787a6dc6f2eae331a1a498ba5b3
Michael Klein <michael@lttl.dev>
2024-12-09 00:01:07 +0100

    remove noop

--- lib/widgets/appbar.dart
+++ lib/widgets/appbar.dart
@@ -134,7 +134,6 @@ class _WyattAppBarState extends ConsumerState<WyattAppBar> {
                     ),
                   ],
                 );
-                ;
               });
         },
       )

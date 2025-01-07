615a1c61012bf1c69727b8537a1feaecfad349e1
Michael Klein <michael@lttl.dev>
2025-01-07 17:51:42 +0100

    add md changelog to webpage

--- lttl.dev/generate.sh
+++ lttl.dev/generate.sh
@@ -4,4 +4,5 @@ which yq > /dev/null || { echo "yq not found, please install it."; exit 1; }
 
 cd "$(dirname "$0")"
 VERSION=$(yq '.version' ../pubspec.yaml)
-m4 -D __VERSION__=${VERSION} index.html.template > wyatt/index.html
+CHANGELOG=$(./print-changelog-as-md.sh)
+m4 -D __VERSION__=${VERSION} -D __CHANGELOG__="${CHANGELOG}" index.template.html > wyatt/index.html
---
+++ lttl.dev/print-changelog-as-md.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for TAG in $(git tag --sort=-creatordate)
+do
+    TAG_DATE=$(git log -1 --pretty=format:'%ad' --date=short ${TAG})
+    echo "**${TAG} (${TAG_DATE})**"
+    TAG_COMMIT_MSG=$(git log -1 ${TAG} --pretty=format:'%s' --no-merges --no-color)
+    echo "- ${TAG_COMMIT_MSG}"
+done

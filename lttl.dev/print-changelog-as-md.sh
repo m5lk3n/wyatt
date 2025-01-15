8f5b0a2ce6cfc6fd189e057f9d4b0c7d52299c02
Michael Klein <michael@lttl.dev>
2025-01-15 18:57:28 +0100

    max tag entries

--- lttl.dev/print-changelog-as-md.sh
+++ lttl.dev/print-changelog-as-md.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+MAX_TAGS=10
+i=0
+
 for TAG in $(git tag --sort=-creatordate)
 do
     TAG_DATE=$(git log -1 --pretty=format:'%ad' --date=short ${TAG})
@@ -7,4 +10,6 @@ do
     TAG_COMMIT_MSG=$(git log -1 ${TAG} --pretty=format:'%s' --no-merges --no-color)
     echo "- ${TAG_COMMIT_MSG}"
     echo
+    i=$((i+1))
+    [ $i -eq $MAX_TAGS ] && break
 done
